b1f9c13d80ac7309db726127e8f899f13915e60e
Shae Bolt <Shae Bolt>
2016-12-06 22:27:05 -0600

    added skeleton 2 image

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import argparse
 import cv2
+import test
 
 # http://stackoverflow.com/questions/16665742/a-good-approach-for-detecting-lines-in-an-image
 # http://felix.abecassis.me/2011/09/opencv-morphological-skeleton/
@@ -10,12 +11,14 @@ import cv2
 # CV_8UC1 is a 8bit one channel color
 CVINT = "uint8"
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='get config input')
     parser.add_argument('image_path', metavar='path', type=str,
                         help='picture file path')
     args = parser.parse_args()
     return args
+
 
 def get_skeleton(image):
     skel = np.zeros(image.shape, dtype="uint8")
@@ -25,9 +28,9 @@ def get_skeleton(image):
     element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
     done = False
     while not done:
-        #create an eroded version of the image
+        # create an eroded version of the image
         cv2.erode(image, element, eroded)
-        #dilate back out
+        # dilate back out
         cv2.dilate(eroded, element, temp)
         # subtract the dilated version from the original version of the image
         cv2.subtract(image, temp, temp)
@@ -41,31 +44,47 @@ def get_skeleton(image):
         print(nonzero)
     return skel
 
+
 def main():
     # getting initial image
     args = parse_args()
     image_path = args.image_path
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    image = cv2.GaussianBlur(image, (11, 11), 0)
+    image = cv2.GaussianBlur(image, (3, 3), 0)
     cv2.imshow("image", image)
     cv2.waitKey(0)
     # img_blur = cv2.GaussianBlur(image, (3, 3), 0)
     _, white_image = cv2.threshold(image, 170, 255, cv2.THRESH_BINARY)
     cv2.imshow("image", white_image)
     cv2.waitKey(0)
-    white_image = cv2.GaussianBlur(white_image, (5, 5), 10)
-    cv2.imshow("image", white_image)
-    cv2.waitKey(0)
-    _, white_image = cv2.threshold(white_image, 170, 255, cv2.THRESH_BINARY)
-    cv2.imshow("image", white_image)
-    cv2.waitKey(0)
+    # white_image = cv2.GaussianBlur(white_image, (3, 3), 0)
+    # cv2.imshow("image", white_image)
+    # cv2.waitKey(0)
+    # _, white_image = cv2.threshold(white_image, 170, 255, cv2.THRESH_BINARY)
+    # cv2.imshow("image", white_image)
+    # cv2.waitKey(0)
     skeleton = get_skeleton(white_image)
     element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    cv2.dilate(skeleton, element, skeleton, iterations=2)
+    #cv2.dilate(skeleton, element, skeleton, iterations=2)
     cv2.imshow("image", skeleton)
     cv2.waitKey(0)
-    cv2.imwrite("skeleton.png", skeleton)
-
+    cv2.imwrite("skeleton2.png", skeleton)
+    h = test.get_homography()
+    nonzero = cv2.findNonZero(skeleton)
+    new_array = np.zeros((60, 24), dtype="uint8")
+    print(len(nonzero))
+    #
+    # for index in nonzero:
+    #     s = np.row_stack((index.T, 1))
+    #     p = h.dot(s)
+    #     x = int(p[0]/p[2])
+    #     y = int(p[1]/p[2])
+    #     if 0 <= x < new_array.shape[1] and 0 <= y < new_array.shape[0]:
+    #
+    #         index = index.T
+    #         new_array[(y, x)] = skeleton[(index[1], index[0])]
+    # print('done')
+    # cv2.imwrite("garble.png",new_array)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 
 
+def get_homography():
+    MUL = 1
+    src_pts = np.array([[24, 601], [457, 597], [404, 493], [79, 491]])
+    dst_pts = np.array([[2 * MUL, 58 * MUL], [23 * MUL, 58 * MUL], [23 * MUL, 47 * MUL], [2 * MUL, 47 * MUL]])
+    h, status = cv2.findHomography(src_pts, dst_pts)
+    return h
+
 def run():
     MUL = 1
     image = cv2.imread("skeleton.png", cv2.IMREAD_GRAYSCALE)
@@ -16,7 +23,7 @@ def run():
                           [0,0,1]])
 
     h, status = cv2.findHomography(src, dst)
-    im_dst = cv2.warpPerspective(image, h, (24 * MUL, 60 *MUL))
+    im_dst = cv2.warpPerspective(image, h, (24 * MUL, 60 *MUL), flags=cv2.INTER_NEAREST)
     #im_dst[:10, :] = 0
 
     cv2.imwrite("test_line.jpg", im_dst)
